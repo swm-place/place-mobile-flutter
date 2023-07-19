675b2a561f8f4a35b145baaacf3e9533569705b6
K1A2 <jckim0307@gmail.com>
2023-07-20 00:54:26 +0900

    [OURS-168] feat: 로그인 페이지 이메일, 패스워드 입력/svg 아이콘 추가

--- lib/login.dart
+++ lib/login.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'theme/text_style.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final newTextTheme = Theme.of(context).textTheme.apply(
+      bodyColor: Colors.pink,
+      displayColor: Colors.pink,
+    );
     return Scaffold(
-      body: Center(
-        child: Text("login"),
-      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(48, 0, 48, 0),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SvgPicture.asset(
+                  'assets/images/navigation_icon.svg',
+                  width: 256,
+                  height: 256,
+                ),
+                const SizedBox(height: 10,),
+                const Text(
+                    "로그인",
+                    style: title_large
+                ),
+                const SizedBox(height: 10,),
+                const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "email",
+                      prefixIcon: Icon(
+                          Icons.mail_rounded
+                      ),
+                    )
+                ),
+                const SizedBox(height: 10,),
+                const TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "password",
+                      prefixIcon: Icon(
+                          Icons.lock_rounded
+                      )
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      )
     );
   }
 }
---
+++ lib/theme/text_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const title_large = TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 22,
+  fontWeight: FontWeight.w400,
+  color: Color(0xff000000),
+  height: 28/22,
+);
