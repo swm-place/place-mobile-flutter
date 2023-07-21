c8a70226d12c9d7dd120f89963160a3b97c2d475
K1A2 <jckim0307@gmail.com>
2023-07-22 02:52:14 +0900

    [OURS-171] feat: 약관 동의 위젯 생성

---
+++ lib/widget/tos.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class CheckTos extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CheckTosState();
+  }
+
+}
+
+class CheckTosState extends State<CheckTos> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
