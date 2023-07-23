efe751c54d1805bdc042c98401f363b9055f9116
K1A2 <jckim0307@gmail.com>
2023-07-23 23:43:11 +0900

    [OURS-175] feat: 비밀번호 찾기 페이지 생성

---
+++ lib/page/find_password.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FindPasswordPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return FindPasswordPageState();
+  }
+}
+
+class FindPasswordPageState extends State<FindPasswordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
