897e118da1726ac70a7ef27f9cdc2e9dc22e3794
K1A2 <jckim0307@gmail.com>
2023-08-06 19:32:27 +0900

    [OURS-226] feat: 장소 카드 태그 칩 파리미터 추가

--- lib/widget/tag/tag_chip.dart
+++ lib/widget/tag/tag_chip.dart
@@ -2,17 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:place_mobile_flutter/theme/text_style.dart';
 
 class TagChip extends StatelessWidget {
+  TagChip({
+    required this.text,
+    this.backgroundColor=Colors.red,
+    Key? key,
+  }) : super(key: key);
+
+  String text;
+  Color backgroundColor;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
-        color: Colors.red
+        color: backgroundColor
       ),
       child: Padding(
         padding: EdgeInsets.fromLTRB(6, 4, 6, 4),
         child: Text(
-          "배고파",
+          text,
           style: placeTagText,
         ),
       ),
