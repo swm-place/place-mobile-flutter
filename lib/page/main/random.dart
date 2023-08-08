6e7c5d95f6f197e40592aeeddfd1df6d4fc7ddca
K1A2 <jckim0307@gmail.com>
2023-08-08 20:22:11 +0900

    [OURS-236] feat: 탐색 이미지 그리드 추가

--- lib/page/main/random.dart
+++ lib/page/main/random.dart
@@ -22,41 +22,48 @@ class RandomPageState extends State<RandomPage> with AutomaticKeepAliveClientMix
         child: Column(
           children: [
             Padding(
-              padding: EdgeInsets.fromLTRB(24, 24, 24, 24),
+              padding: EdgeInsets.fromLTRB(12, 12, 24, 12),
               child: TagSearchBar(
                 elevation: 0,
                 borderRadius: 8,
                 hintText: "검색어",
                 fillColor: Colors.grey[200]!,
-                contentPadding: const EdgeInsets.fromLTRB(18, 18, 18, 18),
+                contentPadding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
                 onSuffixIconPressed: () {
                   print("searchbar clicked");
                 },
               ),
             ),
             Expanded(
-              child: GridView.custom(
-                // physics: NeverScrollableScrollPhysics(),
-                // shrinkWrap: true,
-                gridDelegate: SliverQuiltedGridDelegate(
-                    mainAxisSpacing: 6,
-                    repeatPattern: QuiltedGridRepeatPattern.inverted,
-                    crossAxisSpacing: 6,
-                    crossAxisCount: 4,
-                    pattern: [
-                      QuiltedGridTile(2, 2),
-                      QuiltedGridTile(1, 1),
-                      QuiltedGridTile(1, 1),
-                      QuiltedGridTile(1, 2),
-                    ]
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+                child: GridView.custom(
+                  // physics: NeverScrollableScrollPhysics(),
+                  // shrinkWrap: true,
+                  gridDelegate: SliverQuiltedGridDelegate(
+                      mainAxisSpacing: 6,
+                      repeatPattern: QuiltedGridRepeatPattern.inverted,
+                      crossAxisSpacing: 6,
+                      crossAxisCount: 5,
+                      pattern: [
+                        QuiltedGridTile(2, 2),
+                        QuiltedGridTile(1, 1),
+                        QuiltedGridTile(1, 2),
+                        QuiltedGridTile(1, 3),
+                        QuiltedGridTile(2, 1),
+                        QuiltedGridTile(1, 2),
+                        QuiltedGridTile(2, 2),
+                        QuiltedGridTile(1, 2),
+                      ]
+                  ),
+                  childrenDelegate: SliverChildBuilderDelegate(((context, index) {
+                    return ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.network("https://source.unsplash.com/random?sig=$index", fit: BoxFit.cover,),
+                    );
+                  })),
                 ),
-                childrenDelegate: SliverChildBuilderDelegate(((context, index) {
-                  return ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.network("https://source.unsplash.com/random?sig=$index", fit: BoxFit.cover,),
-                  );
-                })),
-              ),
+              )
             )
           ],
         )
