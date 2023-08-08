6484cd6943052b2c59b03d4a5dca924453376958
K1A2 <jckim0307@gmail.com>
2023-08-08 18:11:50 +0900

    [OURS-236] feat: 탐색 페이지 검색바 추가

--- lib/page/main/random.dart
+++ lib/page/main/random.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:place_mobile_flutter/widget/tag/tag_search_bar.dart';
 
 class RandomPage extends StatefulWidget {
   @override
@@ -12,12 +13,36 @@ class RandomPageState extends State<RandomPage> with AutomaticKeepAliveClientMix
   @override
   bool get wantKeepAlive => true;
 
+  List<Widget> _createSection() {
+    List<Widget> sections = [];
+    sections.add(
+      Container(
+        padding: const EdgeInsets.fromLTRB(24, 0, 24, 0),
+        child: TagSearchBar(
+          elevation: 2,
+          borderRadius: 8,
+          hintText: "검색어",
+          contentPadding: const EdgeInsets.fromLTRB(18, 18, 18, 18),
+          onSuffixIconPressed: () {
+            print("searchbar clicked");
+          },
+        ),
+      )
+    );
+    return sections;
+  }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
       body: SafeArea(
-        child: Text('Random'),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(0, 24, 0, 24),
+          child: Column(
+            children: _createSection()
+          ),
+        ),
       ),
     );
   }
