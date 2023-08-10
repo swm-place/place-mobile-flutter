f14b0e0d26dc72e13dace997dd822ea6dd682c91
K1A2 <jckim0307@gmail.com>
2023-08-10 14:37:28 +0900

    [OURS-234] feat: 프로필/닉네임 표기 추가

--- lib/page/main/profile.dart
+++ lib/page/main/profile.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:place_mobile_flutter/page/account/login.dart';
 import 'package:place_mobile_flutter/state/auth_controller.dart';
+import 'package:place_mobile_flutter/theme/text_style.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -15,28 +16,80 @@ class ProfilePageState extends State<ProfilePage> with AutomaticKeepAliveClientM
   @override
   bool get wantKeepAlive => true;
 
+  Widget _createProfileSection() {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.fromLTRB(24, 24, 24, 0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 96,
+            height: 96,
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://source.unsplash.com/random'),
+            ),
+          ),
+          SizedBox(width: 12,),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // SizedBox(height: 12,),
+                Text("닉네임", style: PageTextStyle.headlineBold(Colors.black),),
+                SizedBox(height: 6,),
+                Row(
+                  children: [
+                    Text("팔로워 20", style: SectionTextStyle.labelMedium(Colors.black),),
+                    SizedBox(width: 12,),
+                    Text("팔로잉 20", style: SectionTextStyle.labelMedium(Colors.black),),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    super.build(context);
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: GetBuilder<AuthController>(
-            init: AuthController(),
-            builder: (controller) {
-              if (controller.user.value == null) {
-                return FilledButton(child: Text("로그인"), onPressed: () => {
-                  Get.to(() => LoginPage())
-                });
-              } else {
-                return FilledButton(child: Text("로그아웃"), onPressed: () => {
-                  controller.signOut()
-                });
-              }
-            },
-          )
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              _createProfileSection()
+            ],
+          ),
         ),
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   super.build(context);
+  //   return Scaffold(
+  //     body: SafeArea(
+  //       child: Center(
+  //         child: GetBuilder<AuthController>(
+  //           init: AuthController(),
+  //           builder: (controller) {
+  //             if (controller.user.value == null) {
+  //               return FilledButton(child: Text("로그인"), onPressed: () => {
+  //                 Get.to(() => LoginPage())
+  //               });
+  //             } else {
+  //               return FilledButton(child: Text("로그아웃"), onPressed: () => {
+  //                 controller.signOut()
+  //               });
+  //             }
+  //           },
+  //         )
+  //       ),
+  //     ),
+  //   );
+  // }
 }
