10ce475a8232bfa0119ca96d5ea68d429fa50912
K1A2 <jckim0307@gmail.com>
2023-08-17 20:03:46 +0900

    [OURS-281] feat: 위도/경도로 직선거리 구하는 함수 추가

--- lib/state/place_controller.dart
+++ lib/state/place_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 
@@ -7,6 +9,26 @@ class PlaceController extends GetxController {
   Rxn<Position> position = Rxn(null);
 
   bool gpsPermissionAllow = false;
+
+  double _degreesToRadians(double degrees) {
+    return degrees * pi / 180;
+  }
+
+  double haversineDistance(double lat1, double lon1, double lat2, double lon2) {
+    const R = 6371000.0;
+
+    var dLat = _degreesToRadians(lat2 - lat1);
+    var dLon = _degreesToRadians(lon2 - lon1);
+
+    lat1 = _degreesToRadians(lat1);
+    lat2 = _degreesToRadians(lat2);
+
+    var a = sin(dLat / 2) * sin(dLat / 2) +
+        sin(dLon / 2) * sin(dLon / 2) * cos(lat1) * cos(lat2);
+    var c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+    return R * c;
+  }
 
   Future<Position?> getPosition() async {
     Position? position;
