685e039118fea95403aa9f63f240477ead8428b6
K1A2 <jckim0307@gmail.com>
2023-08-17 11:30:35 +0900

    [OURS-281] feat: 장소 추천 섹션 api 요청 함수 추가

--- lib/api/provider/place_provider.dart
+++ lib/api/provider/place_provider.dart
@@ -1,5 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
 import 'package:place_mobile_flutter/api/provider/default_provider.dart';
 
 class UserProvider extends DefaultProvider {
+  Future<Map<String, dynamic>?> getPlacRecommandSection() async {
+    Uri uri = Uri.parse("$baseUrl/api-recommender/recommendation/collection/preset");
+    Response response;
+    try {
+      response = await get(uri, headers: setHeader(null));
+    } catch(e) {
+      return null;
+    }
 
+    if (response.statusCode == 200) {
+      return jsonDecode(utf8.decode(response.bodyBytes));
+    } else {
+      return null;
+    }
+  }
 }
