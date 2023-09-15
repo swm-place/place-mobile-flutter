c52ffd44c74fb62e7130bb3bf7bec17b50609959
K1A2 <jckim0307@gmail.com>
2023-09-15 17:01:07 +0900

    [OURS-331] feat: 매거진 테스트 데이터 작성 및 동적 생성 메커니즘 추가

--- lib/page/magazine/magazine.dart
+++ lib/page/magazine/magazine.dart
@@ -17,13 +17,57 @@ class _MagazineState extends State<Magazine> {
 
   bool likeClicked = false;
 
+  final Map<String, dynamic> _magazineContents = {
+    'title': '서울에서 느끼는 자연 코스로 즐기기',
+    'author': {
+      'name': "김준철",
+      'profileUrl': 'https://source.unsplash.com/random?seq=2'
+    },
+    'createdAt': '2022.01.01',
+    'contents': [
+      {
+        'type': 'text',
+        'content': '되는 인간이 사람은 인류의 봄바람이다. 피가 길을 봄날의 끓는 트고, 때까지 같지 '
+            '돋고, 인간의 것이다. 풀밭에 고행을 피가 창공에 같지 안고, 구하지 끝까지 이상을 부패뿐이다.'
+            ' 오직 가지에 청춘 같지 따뜻한 사람은 풀이 약동하다. 사랑의 인간은 대한 물방아 긴지라 '
+            '고행을 수 시들어 없으면 것이다. 가슴이 얼마나 밥을 우리의 희망의 뜨고, 지혜는 풀이 '
+            '아름다우냐? 몸이 사랑의 불러 구하지 새 사라지지 이것은 뿐이다. 크고 피는 싹이 것이다. '
+            '석가는 심장은 방지하는 소금이라 가치를 귀는 튼튼하며, 이것이다. 바이며, 천지는 없으면, '
+            '그들의 않는 이상이 피어나기 봄바람이다.'
+      },
+      {
+        'type': 'divider'
+      },
+      {
+        'type': 'placeCard',
+        'title': '장소 1',
+        'placeUrl': '',
+        'placePicture': 'https://source.unsplash.com/random',
+        'placeContent': '이름자를 이제 이런 다 소학교 말 이런 까닭이요, 있습니다. 옥 가을로 무성할'
+            ' 우는 하나에 하나에 이름과 당신은 까닭입니다. 이네들은 지나가는 헤는 언덕 노새, 가을로 '
+            '봅니다. 릴케 이름과 때 위에 있습니다. 가슴속에 별들을 아무 불러 쓸쓸함과 추억과 별빛이 '
+            '애기 있습니다. 무덤 이름을 하늘에는 딴은 아무 자랑처럼 아침이 봅니다.'
+      },
+      {
+        'type': 'placeCard',
+        'title': '장소 2',
+        'placeUrl': '',
+        'placePicture': 'https://source.unsplash.com/random',
+        'placeContent': '이름자를 이제 이런 다 소학교 말 이런 까닭이요, 있습니다. 옥 가을로 무성할'
+            ' 우는 하나에 하나에 이름과 당신은 까닭입니다. 이네들은 지나가는 헤는 언덕 노새, 가을로 '
+            '봅니다. 릴케 이름과 때 위에 있습니다. 가슴속에 별들을 아무 불러 쓸쓸함과 추억과 별빛이 '
+            '애기 있습니다. 무덤 이름을 하늘에는 딴은 아무 자랑처럼 아침이 봅니다.'
+      }
+    ]
+  };
+
   Widget _createHeader() => Padding(
-    padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
+    padding: const EdgeInsets.fromLTRB(24, 18, 24, 0),
     child: Column(
       children: [
         SizedBox(
           width: double.infinity,
-          child: Text("매거진 제목", style: PageTextStyle.headlineBold(Colors.black)),
+          child: Text(_magazineContents['title'], style: PageTextStyle.headlineBold(Colors.black)),
         ),
         const SizedBox(height: 10),
         Row(
@@ -31,17 +75,36 @@ class _MagazineState extends State<Magazine> {
           children: [
             CircleAvatar(
               radius: 18,
-              backgroundImage: NetworkImage('https://source.unsplash.com/random'),
+              backgroundImage: NetworkImage(_magazineContents['author']['profileUrl']),
             ),
             const SizedBox(width: 10,),
-            Text("작성자", style: SectionTextStyle.sectionContent(Colors.black),),
+            Text(_magazineContents['author']['name'], style: SectionTextStyle.sectionContent(Colors.black),),
             const SizedBox(width: 10,),
-            Text("2022.01.01", style: SectionTextStyle.labelMedium(Colors.grey),),
+            Text(_magazineContents['createdAt'], style: SectionTextStyle.labelMedium(Colors.grey),),
           ],
         )
       ],
     ),
   );
+
+  Widget _createTextContentSection(String content) => Container(
+    padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
+    width: double.infinity,
+    child: Text(content),
+  );
+
+  List<Widget> _createMagazineSection() {
+    List<Widget> section = [_createHeader()];
+    for (Map<String, dynamic> i in _magazineContents['contents']) {
+      switch(i['type']) {
+        case 'text': {
+          section.add(_createTextContentSection(i['content']));
+          break;
+        }
+      }
+    }
+    return section;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -89,9 +152,7 @@ class _MagazineState extends State<Magazine> {
                   flexibleSpace: const PictureFlexibleSpace(),
                 ),
                 SliverList(
-                  delegate: SliverChildListDelegate([
-                    _createHeader()
-                  ]),
+                  delegate: SliverChildListDelegate(_createMagazineSection()),
                 )
               ],
             );
