a91eaadc1d44bc6cb3f0b0ff9cf879f957f53431
K1A2 <jckim0307@gmail.com>
2023-09-17 18:59:17 +0900

    [OURS-335] feat: 코스 메인화면 sliver appbar 추가

--- lib/page/course/course_main.dart
+++ lib/page/course/course_main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:place_mobile_flutter/widget/section/topbar/picture_flexible.dart';
+import 'package:place_mobile_flutter/widget/section/topbar/topbar_flexible_button.dart';
 
 class CourseMainPage extends StatefulWidget {
   const CourseMainPage({
@@ -13,8 +18,39 @@ class _CourseMainPageState extends State<CourseMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text('course'),
+      // extendBodyBehindAppBar: true,
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            // print(constraints.maxWidth);
+            double commentHeight = 58640 / (constraints.maxWidth - 96);
+            return CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  leading: FlexibleTopBarActionButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    icon: Icon(
+                      Platform.isAndroid ? Icons.arrow_back : Icons.arrow_back_ios,
+                      size: 18,
+                    ),
+                    iconPadding: Platform.isAndroid ? EdgeInsets.zero : const EdgeInsets.fromLTRB(6, 0, 0, 0),
+                  ),
+                  pinned: true,
+                  expandedHeight: 220.0,
+                  surfaceTintColor: Colors.white,
+                  backgroundColor: Colors.white,
+                  flexibleSpace: const PictureFlexibleSpace(),
+                ),
+                SliverList(
+                  delegate: SliverChildListDelegate([
+
+                  ]),
+                )
+              ],
+            );
+          },
+        )
     );
   }
 }
