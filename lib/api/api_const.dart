261df18e79a44de96968a8c7aec5ab9a59c637c7
K1A2 <jckim0307@gmail.com>
2023-09-22 14:27:25 +0900

    [OURS-335] refactor: 코스 관련 api 변수 수정

--- lib/api/api_const.dart
+++ lib/api/api_const.dart
@@ -1,3 +1,3 @@
 final baseUrlDev = 'https://been-dev.yeoksi.com';
-final mapBaseUrl = 'http://localhost:8080';
-final routeBaseUrl = 'http://localhost:50000';
+final mapBaseUrl = 'http://192.168.0.2:8080';
+final routeBaseUrl = 'http://192.168.0.2:50000';
