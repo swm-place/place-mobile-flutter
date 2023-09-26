5040387b4f2418ed6866fc279aca964be7ec1858
K1A2 <jckim0307@gmail.com>
2023-09-26 15:21:17 +0900

    [OURS-335] feat: api 포트 정보 변경

--- lib/api/api_const.dart
+++ lib/api/api_const.dart
@@ -1,4 +1,4 @@
 final baseUrlDev = 'https://been-dev.yeoksi.com';
-final mapBaseUrl = 'http://192.168.0.2:8080';
+final mapBaseUrl = 'http://192.168.0.2:49999';
 final routeBaseUrl = 'http://192.168.0.2:50000';
-final geocodingBaseUrl = 'http://192.168.0.2:8000';
+final geocodingBaseUrl = 'http://192.168.0.2:50001';
