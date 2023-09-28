2dbec6b0e95e484dc4151ecfe10149951e613c2d
K1A2 <jckim0307@gmail.com>
2023-09-28 17:50:41 +0900

    [OURS-306] refactor: api route 설정 변경

--- lib/api/api_const.dart
+++ lib/api/api_const.dart
@@ -1,4 +1,7 @@
 final baseUrlDev = 'https://been-dev.yeoksi.com';
-final mapBaseUrl = 'http://192.168.0.2:49999';
-final routeBaseUrl = 'http://192.168.0.2:50000';
-final geocodingBaseUrl = 'http://192.168.0.2:50001';
+// final mapBaseUrl = 'http://192.168.0.2:49999';
+// final routeBaseUrl = 'http://192.168.0.2:50000';
+// final geocodingBaseUrl = 'http://192.168.0.2:50001';
+final mapBaseUrl = 'http://localhost:49999';
+final routeBaseUrl = 'http://localhost:50000';
+final geocodingBaseUrl = 'http://localhost:50001';
