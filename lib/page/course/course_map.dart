4f39472941af52e50a45fd060ce7244642711fa7
K1A2 <jckim0307@gmail.com>
2023-10-01 11:29:21 +0900

    [OURS-306] feat: 상세보기 지도 카드 카루셀로 동작하도록 수정

--- lib/page/course/course_map.dart
+++ lib/page/course/course_map.dart
@@ -1,9 +1,14 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:place_mobile_flutter/api/api_const.dart';
+import 'package:place_mobile_flutter/state/course_controller.dart';
+import 'package:place_mobile_flutter/state/place_controller.dart';
 import 'package:place_mobile_flutter/util/cache/map/map_cache_manager.dart';
 import 'package:place_mobile_flutter/util/map/map_tile_cache.dart';
+import 'package:place_mobile_flutter/util/utility.dart';
+import 'package:place_mobile_flutter/widget/place/place_card.dart';
 import 'package:vector_map_tiles/vector_map_tiles.dart';
 import 'package:latlong2/latlong.dart';
 
@@ -17,18 +22,23 @@ class CourseMapPage extends StatefulWidget {
 }
 
 class _CourseMapPageState extends State<CourseMapPage> {
-  final MapController _controller = MapController();
+  late final MapController _mapController;
+  late final CarouselController _carouselController;
+
   late final CacheManager cacheManager;
 
   @override
   void initState() {
     cacheManager = MapCacheManager.instance;
+    _mapController = MapController();
+    _carouselController = CarouselController();
     super.initState();
   }
 
   @override
   void dispose() {
     cacheManager.dispose();
+    _mapController.dispose();
     super.dispose();
   }
 
@@ -39,7 +49,7 @@ class _CourseMapPageState extends State<CourseMapPage> {
       body: Stack(
         children: [
           FlutterMap(
-            mapController: _controller,
+            mapController: _mapController,
             options: MapOptions(
                 center: const LatLng(37.5036, 127.0448),
                 zoom: 12,
@@ -59,12 +69,42 @@ class _CourseMapPageState extends State<CourseMapPage> {
             ],
           ),
           Positioned(
-            bottom: 0,
+            bottom: 24,
+            left: 0,
+            right: 0,
             child: SafeArea(
-              child: Container(
-                width: 800,
-                height: 200,
-                color: Colors.blue,
+              child: CarouselSlider.builder(
+                options: CarouselOptions(
+                  initialPage: 0,
+                  autoPlay: false,
+                  enableInfiniteScroll: false,
+                  height: 90,
+                  viewportFraction: 0.85,
+                  onPageChanged: (index, reason) {
+                    print(index);
+                  }
+                ),
+                carouselController: _carouselController,
+                itemCount: CourseController.to.coursePlaceData.length,
+                itemBuilder: (context, index, realIndex) {
+                  int? distance;
+                  if (PlaceController.to.userPosition.value != null) {
+                    double lat2 = CourseController.to.coursePlaceData[index]['location']['lat'];
+                    double lon2 = CourseController.to.coursePlaceData[index]['location']['lon'];
+                    distance = PlaceController.to.haversineDistance(lat2, lon2);
+                  }
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
+                    child: RoundedRowRectanglePlaceCard(
+                      imageUrl: CourseController.to.coursePlaceData[index]['imageUrl'],
+                      tags: CourseController.to.coursePlaceData[index]['tags'],
+                      placeName: CourseController.to.coursePlaceData[index]['placeName'],
+                      placeType: CourseController.to.coursePlaceData[index]['placeType'],
+                      open: CourseController.to.coursePlaceData[index]['open'],
+                      distance: distance == null ? null : UnitConverter.formatDistance(distance),
+                    ),
+                  );
+                },
               ),
             )
           )
