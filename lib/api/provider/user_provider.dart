b82abcbc00a982f15ec510c5c0fca74b079cc42d
K1A2 <jckim0307@gmail.com>
2023-11-05 03:26:50 +0900

    [OURS-389] feat: 토큰 저장 및 이용 방식 변경

--- lib/api/provider/user_provider.dart
+++ lib/api/provider/user_provider.dart
@@ -13,7 +13,7 @@ class UserProvider extends DefaultProvider {
     Uri uri = Uri.parse("$baseUrl/api/user/${uid}");
     Response response;
     try {
-      response = await get(uri, headers: setHeader(null));
+      response = await get(uri, headers: await setHeader(false));
     } catch(e) {
       return null;
     }
@@ -27,7 +27,7 @@ class UserProvider extends DefaultProvider {
     // _progressDialogHelper.showProgressDialog('약관 정보 가져오는중');
     print('open');
     try {
-      response = await get(uri, headers: setHeader(null));
+      response = await get(uri, headers: await setHeader(false));
       // _progressDialogHelper.hideProgressDialog();
       print('close1');
     } catch(e) {
@@ -50,7 +50,7 @@ class UserProvider extends DefaultProvider {
 
     _progressDialogHelper.showProgressDialog('닉네임 중복 검사중');
     try {
-      response = await get(uri, headers: setHeader(null));
+      response = await get(uri, headers: await setHeader(false));
       _progressDialogHelper.hideProgressDialog();
     } catch(e) {
       return null;
@@ -58,11 +58,11 @@ class UserProvider extends DefaultProvider {
     return response.statusCode;
   }
 
-  Future<int?> createProfile(Map<String, dynamic> profileData, String token) async {
+  Future<int?> createProfile(Map<String, dynamic> profileData) async {
     User? user = AuthController.to.user.value;
     if (user != null) {
       Uri uri = Uri.parse("$baseUrl/api/user");
-      Map<String, String>? header = setHeader(token);
+      Map<String, String>? header = await setHeader(true);
       header!["Content-Type"] = 'application/json';
 
       Response response;
@@ -76,11 +76,11 @@ class UserProvider extends DefaultProvider {
     return null;
   }
 
-  Future<int?> patchProfile(Map<String, dynamic> profileData, String token) async {
+  Future<int?> patchProfile(Map<String, dynamic> profileData) async {
     User? user = AuthController.to.user.value;
     if (user != null) {
       Uri uri = Uri.parse("$baseUrl/api/user");
-      Map<String, String>? header = setHeader(token);
+      Map<String, String>? header = await setHeader(true);
       header!["Content-Type"] = 'application/json';
 
       Response response;
