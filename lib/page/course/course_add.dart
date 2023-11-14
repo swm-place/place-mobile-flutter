dd1dff6111f47bfcbb482e682734c6921958f9d1
K1A2 <jckim0307@gmail.com>
2023-11-14 21:39:06 +0900

    [OURS-389] feat: 코스 편집 리스트로 변경

--- lib/page/course/course_add.dart
+++ lib/page/course/course_add.dart
@@ -21,64 +21,7 @@ class _CourseAddPageState extends State<CourseAddPage> {
     // {},
   ];
 
-  List<Map<String, dynamic>> _places = [
-    {
-      "imageUrl": "https://source.unsplash.com/random?seq=2",
-      "placeName": "날쏘고가라",
-      "placeType": "레포츠",
-      "location": {
-        'lat': 37.553979,
-        'lon': 126.922668
-      },
-      "open": "영업중",
-      "tags": [
-        {"text": "조용한", "color": RandomGenerator.generateRandomDarkHexColor()},
-        {"text": "넓은", "color": RandomGenerator.generateRandomDarkHexColor()},
-      ]
-    },
-    {
-      "imageUrl": "https://source.unsplash.com/random?seq=3",
-      "placeName": "니컷네컷 홍대점",
-      "placeType": "사진",
-      "location": {
-        'lat': 37.554218,
-        'lon': 126.922398
-      },
-      "open": "영업중",
-      "tags": [
-        {"text": "조용한", "color": RandomGenerator.generateRandomDarkHexColor()},
-        {"text": "넓은", "color": RandomGenerator.generateRandomDarkHexColor()},
-      ]
-    },
-    {
-      "imageUrl": "https://source.unsplash.com/random?seq=4",
-      "placeName": "무신사 테라스 홍대",
-      "placeType": "옷가게",
-      "location": {
-        'lat': 37.557574,
-        'lon': 126.926882
-      },
-      "open": "영업중",
-      "tags": [
-        {"text": "조용한", "color": RandomGenerator.generateRandomDarkHexColor()},
-        {"text": "넓은", "color": RandomGenerator.generateRandomDarkHexColor()},
-      ]
-    },
-    {
-      "imageUrl": "https://source.unsplash.com/random?seq=5",
-      "placeName": "산울림1992",
-      "placeType": "주점",
-      "location": {
-        'lat': 37.554666,
-        'lon': 126.930591
-      },
-      "open": "영업중",
-      "tags": [
-        {"text": "조용한", "color": RandomGenerator.generateRandomDarkHexColor()},
-        {"text": "넓은", "color": RandomGenerator.generateRandomDarkHexColor()},
-      ]
-    },
-  ];
+  List<Map<String, dynamic>> _places = [];
 
   @override
   Widget build(BuildContext context) {
@@ -157,54 +100,29 @@ class _CourseAddPageState extends State<CourseAddPage> {
               ),
               const SizedBox(height: 12,),
               Expanded(
-                child: SingleChildScrollView(
-                  child: Container(
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        RoundedRowRectanglePlaceCard(
-                          imageUrl: _places[0]['imageUrl'],
-                          tags: _places[0]['tags'],
-                          placeName: _places[0]['placeName'],
-                          placeType: _places[0]['placeType'],
-                          open: _places[0]['open'],
-                          distance: '1.3km',
-                          elevation: 0,
-                          borderRadius: 0,
-                          imageBorderRadius: 8,
-                          imagePadding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
-                        ),
-                        RoundedRowRectanglePlaceCard(
-                          imageUrl: _places[0]['imageUrl'],
-                          tags: _places[0]['tags'],
-                          placeName: _places[0]['placeName'],
-                          placeType: _places[0]['placeType'],
-                          open: _places[0]['open'],
-                          distance: '1.3km',
-                          elevation: 0,
-                          borderRadius: 0,
-                          imageBorderRadius: 8,
-                          imagePadding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
-                        ),
-                        RoundedRowRectanglePlaceCard(
-                          imageUrl: _places[0]['imageUrl'],
-                          tags: _places[0]['tags'],
-                          placeName: _places[0]['placeName'],
-                          placeType: _places[0]['placeType'],
-                          open: _places[0]['open'],
-                          distance: '1.3km',
-                          elevation: 0,
-                          borderRadius: 0,
-                          imageBorderRadius: 8,
-                          imagePadding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
-                        ),
-                      ],
-                    )
-                  ),
+                child: ListView.separated(
+                  itemCount: _places.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return RoundedRowRectanglePlaceCard(
+                      imageUrl: _places[0]['imageUrl'],
+                      tags: _places[0]['tags'],
+                      placeName: _places[0]['placeName'],
+                      placeType: _places[0]['placeType'],
+                      open: _places[0]['open'],
+                      distance: '1.3km',
+                      elevation: 0,
+                      borderRadius: 0,
+                      imageBorderRadius: 8,
+                      imagePadding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const SizedBox(height: 4,);
+                  },
                 ),
               ),
               const SizedBox(height: 12,),
-              Container(
+              SizedBox(
                 width: double.infinity,
                 child: FilledButton(
                     onPressed: () {
