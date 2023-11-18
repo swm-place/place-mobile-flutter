628e0bc20d291060455edc23d4aa970c5d2fc6f2
K1A2 <jckim0307@gmail.com>
2023-11-18 14:58:36 +0900

    [OURS-389] feat: 검색중 피드백 추가

--- lib/page/course/course_add.dart
+++ lib/page/course/course_add.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:place_mobile_flutter/api/api_const.dart';
@@ -101,7 +102,39 @@ class _CourseAddPageState extends State<CourseAddPage> {
                       if (keyword == '') {
                         return;
                       }
-                      List<dynamic>? result = await _placeProvider.getPlaceSearchData('google', null, keyword, 37.574863, 126.977725, 5000);
+
+                      Get.dialog(
+                          const AlertDialog(
+                            contentPadding: EdgeInsets.fromLTRB(32, 24, 32, 24),
+                            actionsPadding: EdgeInsets.zero,
+                            titlePadding: EdgeInsets.zero,
+                            content: Row(
+                              children: [
+                                CircularProgressIndicator(),
+                                SizedBox(width: 24),
+                                Text('검색중'),
+                              ],
+                            ),
+                          ),
+                          barrierDismissible: false
+                      );
+                      _places.clear();
+
+                      Position? position = await GISController.to.getPosition();
+                      DateTime nowTime = DateTime.now();
+                      position ??= Position(
+                          longitude: 126.977725,
+                          latitude: 37.574863,
+                          timestamp: nowTime,
+                          accuracy: 0.0,
+                          altitude: 0.0,
+                          heading: 0.0,
+                          speed: 0.0,
+                          speedAccuracy: 0.0
+                      );
+
+                      List<dynamic>? result = await _placeProvider.getPlaceSearchData('google', null, keyword, position.latitude, position.longitude, 5000);
+
                       if (result != null) {
                         for (int c = 0;c < result.length;c++) {
                           for (int i = 0;i < result[c]['hashtags'].length;i++) {
@@ -119,9 +152,14 @@ class _CourseAddPageState extends State<CourseAddPage> {
                             result[c]['distance'] = GISController.to.haversineDistance(lat2, lon2);
                           }
                         }
+                        Get.back();
+
                         setState(() {
                           _places = result;
                         });
+                      } else {
+                        Get.back();
+                        setState(() {});
                       }
                     },
                   ),
@@ -177,69 +215,73 @@ class _CourseAddPageState extends State<CourseAddPage> {
               ),
               const SizedBox(height: 12,),
               Expanded(
-                child: ListView.separated(
-                  itemCount: _places.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return GestureDetector(
-                      onTap: () {
+                child: _places.isNotEmpty ?
+                  ListView.separated(
+                    itemCount: _places.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return GestureDetector(
+                        onTap: () {
 
-                      },
-                      child: RoundedRowRectangleCartPlaceCard(
-                        imageUrl: _places[index]['photos'] != null && _places[index]['photos'].length > 0 ?
-                          "$baseUrlDev/api-recommender/place-photo/?${_places[index]['photos'][0]['url'].split('?')[1]}&max_width=480" :
-                          null,
-                        tags: _places[index]['hashtags'],
-                        placeName: _places[index]['name'],
-                        placeType: _places[index]['category'],
-                        open: _places[index]['open_now'] != null ? (_places[index]['open_now'] ? '영업중' : '영업중 아님') : null,
-                        distance:_places[index]['distance'] == null ?
-                        null : UnitConverter.formatDistance(_places[index]['distance']),
-                        elevation: 0,
-                        borderRadius: 0,
-                        imageBorderRadius: 8,
-                        imagePadding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
-                        onAddPressed: () {
-                          bool exist = false;
-                          for (int i = 0;i < _selectedAddPlace.length;i++) {
-                            if (_selectedAddPlace[i]['id'] == _places[index]['id']) {
-                              exist = true;
-                              break;
-                            }
-                          }
-                          if (!exist) {
-                            for (int i = 0;i < widget.courseController.coursePlaceData.length;i++) {
-                              if (widget.courseController.coursePlaceData[i]['place']['id'] == _places[index]['id']) {
+                        },
+                        child: RoundedRowRectangleCartPlaceCard(
+                          imageUrl: _places[index]['photos'] != null && _places[index]['photos'].length > 0 ?
+                            "$baseUrlDev/api-recommender/place-photo/?${_places[index]['photos'][0]['url'].split('?')[1]}&max_width=480" :
+                            null,
+                          tags: _places[index]['hashtags'],
+                          placeName: _places[index]['name'],
+                          placeType: _places[index]['category'],
+                          open: _places[index]['open_now'] != null ? (_places[index]['open_now'] ? '영업중' : '영업중 아님') : null,
+                          distance:_places[index]['distance'] == null ?
+                          null : UnitConverter.formatDistance(_places[index]['distance']),
+                          elevation: 0,
+                          borderRadius: 0,
+                          imageBorderRadius: 8,
+                          imagePadding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+                          onAddPressed: () {
+                            bool exist = false;
+                            for (int i = 0;i < _selectedAddPlace.length;i++) {
+                              if (_selectedAddPlace[i]['id'] == _places[index]['id']) {
                                 exist = true;
                                 break;
                               }
                             }
-                          }
-                          if (exist) {
-                            Get.dialog(
-                              AlertDialog(
-                                contentPadding: const EdgeInsets.fromLTRB(32, 24, 32, 24),
-                                titlePadding: EdgeInsets.zero,
-                                content: const Text("해당 장소는 이미 추가 목록에 있거나 코스에 포함된 장소입니다. 그래도 추가하시겠습니까?"),
-                                actions: [
-                                  TextButton(onPressed: () {
-                                      Get.back();
-                                      addPlaceInCandidate(_places[index]);
-                                    }, child: const Text('추가')),
-                                  TextButton(onPressed: () {Get.back();}, child: const Text('취소', style: TextStyle(color: Colors.redAccent),))
-                                ],
-                              ),
-                            );
-                            return;
-                          }
-                          addPlaceInCandidate(_places[index]);
-                        },
-                      ),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const SizedBox(height: 4,);
-                  },
-                ),
+                            if (!exist) {
+                              for (int i = 0;i < widget.courseController.coursePlaceData.length;i++) {
+                                if (widget.courseController.coursePlaceData[i]['place']['id'] == _places[index]['id']) {
+                                  exist = true;
+                                  break;
+                                }
+                              }
+                            }
+                            if (exist) {
+                              Get.dialog(
+                                AlertDialog(
+                                  contentPadding: const EdgeInsets.fromLTRB(32, 24, 32, 24),
+                                  titlePadding: EdgeInsets.zero,
+                                  content: const Text("해당 장소는 이미 추가 목록에 있거나 코스에 포함된 장소입니다. 그래도 추가하시겠습니까?"),
+                                  actions: [
+                                    TextButton(onPressed: () {
+                                        Get.back();
+                                        addPlaceInCandidate(_places[index]);
+                                      }, child: const Text('추가')),
+                                    TextButton(onPressed: () {Get.back();}, child: const Text('취소', style: TextStyle(color: Colors.redAccent),))
+                                  ],
+                                ),
+                              );
+                              return;
+                            }
+                            addPlaceInCandidate(_places[index]);
+                          },
+                        ),
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) {
+                      return const SizedBox(height: 4,);
+                    },
+                  ) :
+                  Center(
+                    child: Text('검색결과 없음'),
+                  ),
               ),
               const SizedBox(height: 12,),
               SizedBox(
@@ -256,7 +298,25 @@ class _CourseAddPageState extends State<CourseAddPage> {
                         );
                         return;
                       }
+
+                      Get.dialog(
+                          const AlertDialog(
+                            contentPadding: EdgeInsets.fromLTRB(32, 24, 32, 24),
+                            actionsPadding: EdgeInsets.zero,
+                            titlePadding: EdgeInsets.zero,
+                            content: Row(
+                              children: [
+                                CircularProgressIndicator(),
+                                SizedBox(width: 24),
+                                Text('장소 추가중'),
+                              ],
+                            ),
+                          ),
+                          barrierDismissible: false
+                      );
+
                       bool result = await widget.courseController.addPlace(_selectedAddPlace);
+                      Get.back();
                       if (result) {
                         Navigator.pop(context);
                       } else {
